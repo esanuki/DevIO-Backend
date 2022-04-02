1e44497453dd28929652e0b2876b66450fd9f7dd
esanuki <esanuki@hotmail.com>
2022-04-02 19:58:23 -0300

    Atualizando API

--- src/DevIO.Data/Migrations/20220308121557_Initial.Designer.cs
+++ src/DevIO.Data/Migrations/20220308121557_Initial.Designer.cs
@@ -4,14 +4,16 @@ using DevIO.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DevIO.Data.Migrations
 {
     [DbContext(typeof(DevIODbContext))]
-    partial class DevIODbContextModelSnapshot : ModelSnapshot
+    [Migration("20220308121557_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
