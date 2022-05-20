e3439737872ef232b421896c1ee07d86421bf72c
esanuki <esanuki@hotmail.com>
2022-05-19 23:53:06 -0300

    Atualizando projeto

--- src/DevIO.Api/Interop/ViewModels/EnderecoViewModel.cs
+++ src/DevIO.Api/Interop/ViewModels/EnderecoViewModel.cs
@@ -8,7 +8,7 @@ namespace DevIO.Api.Interop.ViewModels
 {
     public class EnderecoViewModel
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
--- src/DevIO.Api/Interop/ViewModels/FornecedorViewModel.cs
+++ src/DevIO.Api/Interop/ViewModels/FornecedorViewModel.cs
@@ -5,7 +5,7 @@ namespace DevIO.Api.Interop.ViewModels
 {
     public class FornecedorViewModel
     {
-        public Guid Id { get; set; }
+        public Guid? Id { get; set; }
 
         [Required(ErrorMessage = "O campo {0} é obrigatório")]
         [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
